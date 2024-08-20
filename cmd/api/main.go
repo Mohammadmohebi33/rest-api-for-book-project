bf577aa9f3222408959847dddbd73755f8d1bbb8
Mohammadmohebi33 <mohammad.rasoul.mohebi@gmail.com>
2024-08-20 19:36:53 +0330

    user and token

---
+++ cmd/api/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"vue_api/internal/data"
+	"vue_api/internal/driver"
+)
+
+type config struct {
+	port int
+}
+
+type application struct {
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+	models   data.Models
+}
+
+func main() {
+	var cfg config
+	cfg.port = 8081
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	dsn := "host=localhost port=5432 user=postgres password=password dbname=vueapi sslmode=disable timezone=UTC connect_timeout=5"
+
+	db, err := driver.ConnectPostgres(dsn)
+	if err != nil {
+		log.Fatal("Cannot connect to database")
+	}
+
+	app := &application{
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
+		models:   data.New(db.SQL),
+	}
+
+	err = app.serve()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// serve starts the web server
+func (app *application) serve() error {
+	app.infoLog.Println("API listening on port", app.config.port)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", app.config.port),
+		Handler: app.routes(),
+	}
+
+	return srv.ListenAndServe()
+}
